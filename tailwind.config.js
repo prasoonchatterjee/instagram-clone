8e664d6ba23b7f03e287447cc54c1683736bd000
prasoonchatterjee <50299354+prasoonchatterjee@users.noreply.github.com>
2021-03-03 20:22:34 +0530

    login page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,27 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fill: (theme) => ({
+      red: theme("colors.red.primary"),
+    }),
+    colors: {
+      white: "#ffffff",
+      blue: {
+        medium: "#005c98",
+      },
+      black: {
+        light: "#262626",
+        faded: "#00000059",
+      },
+      gray: {
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
+      },
+      red: {
+        primary: "#ed4956",
+      },
+    },
   },
   variants: {
     extend: {},
