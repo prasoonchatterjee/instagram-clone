0310a841936b9cd17e6ca206cd4ae7b714702fd9
prasoonchatterjee <50299354+prasoonchatterjee@users.noreply.github.com>
2021-03-03 13:42:45 +0530

    tailwindcss setp

--- src/pages/login.js
+++ src/pages/login.js
@@ -17,5 +17,17 @@ export default function Login() {
   useEffect(() => {
     document.title = "Login - Instagram";
   }, []);
-  return <p>im the login page</p>;
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img
+          src="/images/iphone-with-profile.jpg"
+          alt="iPhone with Instagram app"
+        />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>I will be the form!</p>
+      </div>
+    </div>
+  );
 }
