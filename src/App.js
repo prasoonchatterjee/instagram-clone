9d9537cf6e5f8713e2c0f652bbcb23e7fe33f64e
prasoonchatterjee <50299354+prasoonchatterjee@users.noreply.github.com>
2021-03-07 18:18:15 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import UserContext from "./context/user";
+import useAuthListener from "./hooks/use-auth-listener";
 import * as ROUTES from "./constants/routes";
+
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const NotFound = lazy(() => import("./pages/not-found"));
 
 function App() {
+  const { user } = useAuthListener();
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
