def540a2df32d09ffc6bfa6210803ff56b836f2f
prasoonchatterjee <50299354+prasoonchatterjee@users.noreply.github.com>
2021-04-01 16:03:55 +0530

    development done

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import UserContext from "./context/user";
 import useAuthListener from "./hooks/use-auth-listener";
 import * as ROUTES from "./constants/routes";
 
+import ProtectedRoute from "./helpers/protected-route";
+
 const Login = lazy(() => import("./pages/login"));
 const SignUp = lazy(() => import("./pages/sign-up"));
 const Dashboard = lazy(() => import("./pages/dashboard"));
 const NotFound = lazy(() => import("./pages/not-found"));
+const Profile = lazy(() => import("./pages/profile"));
 
 function App() {
   const { user } = useAuthListener();
@@ -17,9 +20,12 @@ function App() {
       <Router>
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route path={ROUTES.LOGIN} component={Login} />
-            <Route path={ROUTES.SIGN_UP} component={SignUp} />
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <Route path={ROUTES.LOGIN} component={Login} exact />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} exact />
+            <Route path={ROUTES.PROFILE} component={Profile} exact />
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+              <Dashboard />
+            </ProtectedRoute>
             <Route component={NotFound} />
           </Switch>
         </Suspense>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,17 +1,27 @@
-import { useEffect } from "react";
-import { Header, Sidebar, Timeline } from "../components";
+import { useEffect, useContext } from "react";
+import Header from "../components/header";
+import Timeline from "../components/timeline";
+import Sidebar from "../components/sidebar";
+import useUser from "../hooks/use-user";
+import LoggedInUserContext from "../context/logged-in-user";
+import UserContext from "../context/user";
 
 export default function Dashboard() {
+  const logUser = useContext(UserContext);
+  const { user } = useUser(logUser.uid);
   useEffect(() => {
     document.title = "Instagram";
   }, []);
+
   return (
-    <div className="bg-gray-background">
-      <Header />
-      <div className="grid">
-        <Timeline />
-        <Sidebar />
+    <LoggedInUserContext.Provider value={{ user }}>
+      <div className="bg-gray-background">
+        <Header />
+        <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+          <Timeline />
+          <Sidebar />
+        </div>
       </div>
-    </div>
+    </LoggedInUserContext.Provider>
   );
 }
